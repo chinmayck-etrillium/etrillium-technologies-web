7eb8d2658505e421c05f2e7e0bc6f2094a74129f
chinmayck-etrillium <chinmayck24@gmail.com>
2024-05-07 12:14:02 +0530

    Website phase one(basics) complete

---
+++ components/ui/header.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import MobileMenu from "./mobile-menu";
+import { Outfit } from "@next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: "600",
+});
+
+export default function Header() {
+  return (
+    <>
+      <header className="absolute w-full z-30">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="flex items-center justify-between h-20">
+            {/* Site branding */}
+            <div className="shrink-0 mr-4">
+              {/* Logo */}
+              <Link href="/" className="block">
+                <svg
+                  className="w-10 h-10 fill-current text-purple-600"
+                  viewBox="0 0 40 40"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle fill="#6A28E2" cx="20" cy="10" r="9" />
+
+                  <circle
+                    fill="rgba(106, 40, 226, 0.85)"
+                    cx="12.5"
+                    cy="23"
+                    r="10"
+                  />
+
+                  <circle
+                    fill="rgba(106, 40, 226, 0.65)"
+                    cx="28.5"
+                    cy="23"
+                    r="10"
+                  />
+                </svg>
+              </Link>
+            </div>
+
+            {/* Desktop navigation */}
+            <nav className="hidden md:flex md:grow">
+              {/* Desktop sign in links */}
+              <ul className="flex grow justify-end flex-wrap items-center">
+                <li
+                  className={`${outfit.className} font-large text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out text-lg`}
+                >
+                  ETrillium Technologies
+                </li>
+              </ul>
+            </nav>
+            <MobileMenu />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
---
+++ components/zigzag.tsx
@@ -0,0 +1,220 @@
+import Image from "next/image";
+
+import FeatImage01 from "../public/images/comprehensive-solutions.jpg";
+import FeatImage02 from "../public/images/personalised-guidance.jpg";
+import FeatImage03 from "../public/images/streamlined-experience.jpg";
+
+export default function Zigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+              Reach goals that matter
+            </div>
+            <h1 className="h2 mb-4">Your One-Stop Financial Hub</h1>
+            <p className="text-xl text-gray-400">
+              Our one-stop financial solution streamlines wealth management,
+              investment planning, retirement, tax optimization, insurance, and
+              estate planning into a cohesive platform, ensuring convenience and
+              efficiency for all your financial needs.
+            </p>
+          </div>
+
+          {/* Items */}
+          <div className="grid gap-20">
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage01}
+                  width={540}
+                  height={405}
+                  alt="Features 01"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    Amplify growth, minimize costs.
+                  </div>
+                  <h3 className="h3 mb-3">Comprehensive Solutions</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    Our product offers a holistic approach, covering all aspects
+                    of financial management, from wealth growth to risk
+                    mitigation and legacy planning.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Integrated Approach</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Efficiency and Convenience</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Better Decision-Making</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage02}
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    Amplify growth, minimize costs.
+                  </div>
+                  <h3 className="h3 mb-3">Personalized Guidance</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    We provide tailored advice and strategies based on
+                    individual needs and goals, ensuring that each client
+                    receives personalized attention and support.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Adaptability</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Tailored Strategies</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Emotional Support</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage03}
+                  width={540}
+                  height={405}
+                  alt="Features 03"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    Amplify growth, minimize costs.
+                  </div>
+                  <h3 className="h3 mb-3">Streamlined Experience</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    With our intuitive platform and expert guidance, users can
+                    navigate complex financial decisions with ease, saving time
+                    and minimizing stress.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Enhanced Control and Organization</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Clarity and Transparency</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
