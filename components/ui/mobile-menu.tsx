caeb591c079dfb95b33e42211493bf3d5afcf931
chinmayck-etrillium <chinmayck24@gmail.com>
2024-05-07 13:11:41 +0530

    Mobile Responsiveness updated

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -1,80 +1,31 @@
-'use client'
+"use client";
 
-import { useState, useRef, useEffect } from 'react'
-import Link from 'next/link'
+import { useState, useEffect } from "react";
+import { Outfit } from "@next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: "600",
+});
 
 export default function MobileMenu() {
-  const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false)
+  const [isHomepage, setIsHomepage] = useState(false);
 
-  const trigger = useRef<HTMLButtonElement>(null)
-  const mobileNav = useRef<HTMLDivElement>(null)
-
-  // close the mobile menu on click outside
   useEffect(() => {
-    const clickHandler = ({ target }: { target: EventTarget | null }): void => {
-      if (!mobileNav.current || !trigger.current) return;
-      if (!mobileNavOpen || mobileNav.current.contains(target as Node) || trigger.current.contains(target as Node)) return;
-      setMobileNavOpen(false)
-    };
-    document.addEventListener('click', clickHandler)
-    return () => document.removeEventListener('click', clickHandler)
-  })
-
-  // close the mobile menu if the esc key is pressed
-  useEffect(() => {
-    const keyHandler = ({ keyCode }: { keyCode: number }): void => {
-      if (!mobileNavOpen || keyCode !== 27) return;
-      setMobileNavOpen(false)
-    };
-    document.addEventListener('keydown', keyHandler)
-    return () => document.removeEventListener('keydown', keyHandler)
-  })
+    // Determine if we are on the homepage after component mounts
+    setIsHomepage(window.location.pathname === "/");
+  }, []);
 
   return (
     <div className="md:hidden">
-      {/* Hamburger button */}
-      <button
-        ref={trigger}
-        className={`hamburger ${mobileNavOpen && 'active'}`}
-        aria-controls="mobile-nav"
-        aria-expanded={mobileNavOpen}
-        onClick={() => setMobileNavOpen(!mobileNavOpen)}
-      >
-        <span className="sr-only">Menu</span>
-        <svg
-          className="w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
+      {/* Render "Etrillium Technologies" if it's the homepage */}
+      {isHomepage && (
+        <div
+          className={`${outfit.className} font-large text-purple-600 hover:text-gray-200 text-2xl font-bold text-center py-4`}
         >
-          <rect y="4" width="24" height="2" rx="1" />
-          <rect y="11" width="24" height="2" rx="1" />
-          <rect y="18" width="24" height="2" rx="1" />
-        </svg>
-      </button>
-
-      {/*Mobile navigation */}
-      <nav
-        id="mobile-nav"
-        ref={mobileNav}
-        className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out"
-        style={mobileNavOpen ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: 0.8 }}
-      >
-        <ul className="bg-gray-800 px-4 py-2">
-          <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
-              Sign in
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/signup"
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
-            >
-              Sign up
-            </Link>
-          </li>
-        </ul>
-      </nav>
+          Etrillium Technologies
+        </div>
+      )}
     </div>
-  )
+  );
 }
